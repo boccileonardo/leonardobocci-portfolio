e76c1da650da4ac4aa722fb0560bfb945f4baa90
leo <leonardobocci99@hotmail.com>
2023-11-17 11:26:04 +0100

    added azure data fundamentals and fixed carousel responsiveness

--- src/pages/CertificationsCarousel.js
+++ src/pages/CertificationsCarousel.js
@@ -6,7 +6,6 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const CertificationsCarousel = ({ certifications }) => {
-
   const settings = {
     dots: false,
     infinite: true,
@@ -14,20 +13,37 @@ const CertificationsCarousel = ({ certifications }) => {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 2300,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 768, // Adjust this value based on your design needs
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <div className="carousel-wrapper" style={{position: 'relative', marginLeft: '10%', marginRight: '10%',  paddingBottom: '100px'}}>
+    <div className="carousel-wrapper" style={{ position: 'relative', margin: '0 auto', maxWidth: '1080px', paddingBottom: '100px' }}>  
       <Slider {...settings}>
-        {certifications && certifications.map((certification, index) => (
-          <div key={index} style={{ textAlign: 'center', alignItems: 'center'}}>
-          <h3 style={{ textAlign: 'center' }}>{certification.title}</h3>
-          {certification.image && <img src={certification.image} alt={certification.title} style={{ maxHeight: '150px', width: 'auto', alignSelf: 'center', margin: '0 auto', borderRadius: '50%'}} />}
-          <p style={{ textAlign: 'center' }}>{certification.date}</p>
-        </div>
-        ))}
+        {certifications &&
+          certifications.map((certification, index) => (
+            <a href={certification.link} target="_blank" rel="noopener noreferrer" key={index} style={{ textDecoration: 'none' }}>
+              <div style={{ textAlign: 'center', alignItems: 'center' }}>
+                <h3 style={{ textAlign: 'center' }}>{certification.title}</h3>
+                {certification.image && (
+                  <img
+                    src={certification.image}
+                    alt={certification.title}
+                    style={{ maxHeight: '150px', width: 'auto', alignSelf: 'center', margin: '0 auto', borderRadius: '50%' }}
+                  />
+                )}
+                <p style={{ textAlign: 'center' }}>{certification.date}</p>
+              </div>
+            </a>
+          ))}
       </Slider>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,21 +12,21 @@ import {
 
 import CertificationsCarousel from './/CertificationsCarousel.js';
 
-//import carImage from "./car.png";
 import azureFundamentals from "../../content/images/certs/azureFundamentals.png";
+import azureDataFundamentals from "../../content/images/certs/azureDataFundamentals.png";
 import gcpDataInsights from "../../content/images/certs/gcpDataInsights.png";
 import gcpBigQuery from "../../content/images/certs/gcpBigQuery.png";
 import pgPowerBi from "../../content/images/certs/pgPowerBi.png";
 import lakehouseFundamentals from "../../content/images/certs/lakehouseFundamentals.png";
 
 const certifications = [
-    // Your certification data
-     { title: 'Databricks Lakehouse', image: lakehouseFundamentals, date: 'September 2023'},
-     { title: 'Azure Fundamentals', image: azureFundamentals, date: 'January 2023'},
-     { title: 'GCP Data to Insights', image: gcpDataInsights, date: 'March 2022'},
-     { title: 'GCP BigQuery', image: gcpBigQuery, date: 'December 2021'},
-     { title: 'P&G PowerBi', image: pgPowerBi, date: 'December 2021'},
-    // ...
+    // ADD CERTIFICATIONS HERE WHEN ACQUIRED, ALSO ADD IMAGE ABOVE
+     { title: 'Azure Data Fundamentals', image: azureDataFundamentals, date: 'November 2023', link: 'https://learn.microsoft.com/en-us/users/leonardobocci-5506/credentials/2f8cbf5cba1fda61'},
+     { title: 'Databricks Lakehouse', image: lakehouseFundamentals, date: 'September 2023', link: '../../static/lakehouseFundamentals.pdf'},
+     { title: 'Azure Fundamentals', image: azureFundamentals, date: 'January 2023', link: 'https://learn.microsoft.com/api/credentials/share/en-us/LeonardoBocci-5506/B83D7D90AD724399?sharingId=DAFD346806A277E6'},
+     { title: 'GCP Data to Insights', image: gcpDataInsights, date: 'March 2022', link: 'https://www.coursera.org/account/accomplishments/specialization/4H8CRJS6RE86'},
+     { title: 'GCP BigQuery', image: gcpBigQuery, date: 'December 2021', link: 'https://coursera.org/verify/DJJZK5VVECTZ'},
+     { title: 'P&G PowerBi', image: pgPowerBi, date: 'December 2021', link: 'https://www.credly.com/badges/e6b80856-4ecd-4f0a-951f-835af4731d6d'},
   ];
 
 export default function IndexPage() {
@@ -37,7 +37,7 @@ export default function IndexPage() {
         <HeroSection sectionId="hero" />
         <AboutSection sectionId="about" heading="About Me" />
         <h2 style={{ textAlign: 'center', marginTop: '2em' }}>Certifications</h2>
-        {certifications && <CertificationsCarousel certifications={certifications} />}
+        <CertificationsCarousel certifications={certifications} />
         <InterestsSection sectionId="interests" heading="Interests" />
         <ProjectsSection sectionId="projects" heading="Featured Projects" />
       </Page>
