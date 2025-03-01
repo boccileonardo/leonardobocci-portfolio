ac28f4ef6cd99f7d69d59ce33626817a0f3fbb3e
leonardobocci <leonardobocci99@hotmail.com>
2025-03-01 13:12:17 +0100

    resume and certificates update

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,23 +12,19 @@ import {
 
 import CertificationsCarousel from './/CertificationsCarousel.js';
 
-import azureFundamentals from "../../content/images/certs/azureFundamentals.png";
+import databricksDataEngineer from "../../content/images/certs/databricksDataEngineer.jpeg";
 import azureDataFundamentals from "../../content/images/certs/azureDataFundamentals.png";
 import gcpDataInsights from "../../content/images/certs/gcpDataInsights.png";
-import gcpBigQuery from "../../content/images/certs/gcpBigQuery.png";
-import pgPowerBi from "../../content/images/certs/pgPowerBi.png";
-import lakehouseFundamentals from "../../content/images/certs/lakehouseFundamentals.png";
 import dagsterEssentials from "../../content/images/certs/dagsterEssentials.png";
+import cdmpAssociate from "../../content/images/certs/cdmpAssociate.png";
 
 const certifications = [
     // ADD CERTIFICATIONS HERE WHEN ACQUIRED, ALSO ADD IMAGE ABOVE
-     { title: 'Dagster Essentials', image: dagsterEssentials, date: 'November 2023', link: 'https://courses.dagster.io/certificates/5gick1z9ja'},
-     { title: 'Azure Data Fundamentals', image: azureDataFundamentals, date: 'November 2023', link: 'https://learn.microsoft.com/en-us/users/leonardobocci-5506/credentials/2f8cbf5cba1fda61'},
-     { title: 'Databricks Lakehouse', image: lakehouseFundamentals, date: 'September 2023', link: '/lakehouseFundamentals.pdf'},
-     { title: 'Azure Fundamentals', image: azureFundamentals, date: 'January 2023', link: 'https://learn.microsoft.com/api/credentials/share/en-us/LeonardoBocci-5506/B83D7D90AD724399?sharingId=DAFD346806A277E6'},
-     { title: 'GCP Data to Insights', image: gcpDataInsights, date: 'March 2022', link: 'https://www.coursera.org/account/accomplishments/specialization/4H8CRJS6RE86'},
-     { title: 'GCP BigQuery', image: gcpBigQuery, date: 'December 2021', link: 'https://coursera.org/verify/DJJZK5VVECTZ'},
-     { title: 'P&G PowerBi', image: pgPowerBi, date: 'December 2021', link: 'https://www.credly.com/badges/e6b80856-4ecd-4f0a-951f-835af4731d6d'},
+    { title: 'Databricks Data Engineer', image: databricksDataEngineer, date: 'June 2024', link: 'https://credentials.databricks.com/1ea4a7d0-03b0-4eab-b042-e1154249f90a#acc.XzZIG4yb'}, 
+    { title: 'Azure Data Fundamentals', image: azureDataFundamentals, date: 'November 2023', link: 'https://learn.microsoft.com/en-us/users/leonardobocci-5506/credentials/2f8cbf5cba1fda61'},
+    { title: 'GCP Data to Insights', image: gcpDataInsights, date: 'March 2022', link: 'https://www.coursera.org/account/accomplishments/specialization/4H8CRJS6RE86'},
+    { title: 'Dagster Essentials', image: dagsterEssentials, date: 'November 2023', link: 'https://courses.dagster.io/certificates/5gick1z9ja'},
+    { title: 'Data Management Professional', image: cdmpAssociate, date: 'July 2024', link: 'https://eu.credential.net/58ecce6e-d538-455a-829e-b11cf92f1603#acc.bzX7xNLD'},
   ];
 
 export default function IndexPage() {
